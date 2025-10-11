547f017946f831e7bc912a251e5f3a4caf50c953
Matthew2839 <matthew2839@postech.ac.kr>
2025-10-12 06:15:49 +0900

    Implemented from in Solver.scala

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -68,7 +68,13 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+    val next = for {
+      init <- initial
+      (blocks, moveLists) <- newNeighborsOnly(neighborsWithHistory(init._1, init._2), explored)
+    } yield (blocks, moveLists)
+    initial #::: from(next, explored ++ next.map(_._1))
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
